6a765481b540cdccfb92602c3c1c372eef29ec39
Tasman Thenell <thenellt@gmail.com>
2017-06-19 11:49:27 -0700

    cosmetic improvements and js cleanup

--- js/floatingContent.js
+++ js/floatingContent.js
@@ -1,58 +1,8 @@
 var logVisibility = 0;
 var backgroundVisibility = 0;
 
-var changelogButton = document.getElementById("changelogButton");
-changelogButton.addEventListener('click', function() {
-        var changeDiv = document.getElementById("changelog");
-        var hidepage = document.getElementById("hidepage");
-        if(backgroundVisibility){
-                var backgroundDiv = document.getElementById("projBackground");
-                backgroundVisibility = 0;
-                logVisibility = 1;
-                fadeOut(backgroundDiv);
-                fadeIn(changeDiv);
-        }
-        else if(logVisibility == 0){
-                console.log("unhiding");
-                logVisibility = 1;
-                fadeIn(changeDiv);
-                fadeIn(hidepage);
-        }
-        else{
-                console.log("hiding");
-                logVisibility = 0;
-                fadeOut(changeDiv);
-                fadeOut(hidepage);
-        }
-});
-
-var changelogButton = document.getElementById("backgroundButton");
-changelogButton.addEventListener('click', function() {
-        var backgroundDiv = document.getElementById("projBackground");
-        var hidepage = document.getElementById("hidepage");
-        if(logVisibility){
-                var changeDiv = document.getElementById("changelog");
-                backgroundVisibility = 1;
-                logVisibility = 0;
-                fadeOut(changeDiv);
-                fadeIn(backgroundDiv);
-        }
-        else if(backgroundVisibility == 0){
-                console.log("unhiding");
-                backgroundVisibility = 1;
-                fadeIn(backgroundDiv);
-                fadeIn(hidepage);
-        }
-        else{
-                console.log("hiding");
-                backgroundVisibility = 0;
-                fadeOut(backgroundDiv);
-                fadeOut(hidepage);
-        }
-});
-
 //pure javascript fade and unfade from: https://stackoverflow.com/questions/6121203/how-to-do-fade-in-and-fade-out-with-javascript-and-css
-function fadeOut(element) {
+var fadeOut = function(element) {
         var op = 1;  // initial opacity
         var timer = setInterval(function () {
                 if (op <= 0.1){
@@ -62,22 +12,86 @@ function fadeOut(element) {
                 element.style.opacity = op;
                 element.style.filter = 'alpha(opacity=' + op * 100 + ")";
                 op -= op * 0.1;
-        }, 20);
+        }, 10);
 }
 
-function fadeIn(element) {
+var fadeIn = function(element) {
         var op = 0.1;  // initial opacity
         element.style.opacity = op;
         element.style.visibility = "visible";
         var timer = setInterval(function () {
-                if (op >= 1){
+                if (op >= .8){
                         clearInterval(timer);
                 }
                 element.style.opacity = op;
                 element.style.filter = 'alpha(opacity=' + op * 100 + ")";
                 op += op * 0.1;
-        }, 20);
+        }, 10);
 }
+
+var toggleChangelog = function() {
+        console.log("toggleChangelog called");
+        var changeDiv = document.getElementById('changelogContent')
+        var hidepage = document.getElementById("hidepage");
+        if(backgroundVisibility){
+                var backgroundDiv = document.getElementById("backgroundContent");
+                backgroundVisibility = 0;
+                logVisibility = 1;
+                backgroundDiv.classList.add('scale-out');
+                backgroundDiv.classList.remove('scale-in');
+                changeDiv.classList.add('scale-in');
+                changeDiv.classList.remove('scale-out');
+        }
+        else if(logVisibility == 0){
+                console.log("unhiding");
+                logVisibility = 1;
+                fadeIn(hidepage);
+                changeDiv.classList.add('scale-in');
+                changeDiv.classList.remove('scale-out');
+        }
+        else{
+                console.log("hiding");
+                logVisibility = 0;
+                changeDiv.classList.add('scale-out');
+                changeDiv.classList.remove('scale-in');
+                fadeOut(hidepage);
+        }
+}
+
+var changelogButton = document.getElementById("changelogButton");
+changelogButton.addEventListener('click', toggleChangelog);
+
+var toggleBackground = function() {
+        console.log("toggleBackground called");
+        var backgroundDiv = document.getElementById("backgroundContent");
+        var hidepage = document.getElementById("hidepage");
+        if(logVisibility){
+                var changeDiv = document.getElementById("changelogContent");
+                backgroundVisibility = 1;
+                logVisibility = 0;
+                backgroundDiv.classList.add('scale-in');
+                backgroundDiv.classList.remove('scale-out');
+                changeDiv.classList.add('scale-out');
+                changeDiv.classList.remove('scale-in');
+        }
+        else if(backgroundVisibility == 0){
+                console.log("unhiding");
+                backgroundVisibility = 1;
+                backgroundDiv.classList.add('scale-in');
+                backgroundDiv.classList.remove('scale-out');
+                fadeIn(hidepage);
+        }
+        else{
+                console.log("hiding");
+                backgroundVisibility = 0;
+                backgroundDiv.classList.add('scale-out');
+                backgroundDiv.classList.remove('scale-in');
+                fadeOut(hidepage);
+        }
+}
+
+var backgroundButton = document.getElementById("backgroundButton");
+backgroundButton.addEventListener('click', toggleBackground);
 
 //taken from a handy stack overflow: https://stackoverflow.com/questions/27840222/how-can-i-load-the-contents-of-a-small-text-file-into-a-javascript-var-wo-jquery
 var readLocalFile = function (url, type, callback) {
@@ -113,7 +127,7 @@ readLocalFile("./changelog.txt", 'GET', function(responseText) {
 
                 list.appendChild(item);
         }
-        
+
         document.getElementById("changelogTitle").appendChild(document.createTextNode("Project Change Log:"));
         document.getElementById('changelogContent').appendChild(list);
 });
