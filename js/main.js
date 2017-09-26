6dc9f678b59c9ba88f4b90396cf8b0fd484c9c50
Tasman Thenell <thenellt@oregonstate.edu>
2017-09-26 14:19:49 -0700

    more save load work for persistent storage

--- js/main.js
+++ js/main.js
@@ -292,3 +292,4 @@ function checkSettings(){
 }
 
 setupTabs();
+
--- jsDev/saveLoad.js
+++ jsDev/saveLoad.js
@@ -82,6 +82,19 @@ function parseConfigFile(fileString){
 }
 
 function saveSimToFile(saveTowns){
+        var saveObject = generateConfigObject();
+        
+        //save populations
+        if(saveTowns){
+                console.log("adding populations to file");
+        }
+
+        var outputString = JSON.stringify(saveObject);
+        var jsonBlob = new Blob([outputString], {type: "application/json"});
+        saveAs(jsonBlob, simName + ".cfg");
+}
+
+function generateConfigObject(){
         var saveObject = {};
         saveObject.simName = simName;
         saveObject.animalDiffRate = animalDiffRate;
@@ -97,14 +110,7 @@ function saveSimToFile(saveTowns){
         saveObject.lowColorCode = lowColorCode;
         saveObject.highColorCode = highColorCode;
         
-        //save populations
-        if(saveTowns){
-                console.log("adding populations to file");
-        }
-
-        var outputString = JSON.stringify(saveObject);
-        var jsonBlob = new Blob([outputString], {type: "application/json"});
-        saveAs(jsonBlob, simName + ".cfg");
+        return saveObject;
 }
 
 function saveImgToFile(type){
@@ -125,4 +131,68 @@ function saveImgToFile(type){
         }
 }
 
+function generatePersistObject(saveTowns){
+        var saveObject = generateConfigObject();
+        var persistObject = {};
+        
+        persistObject.name = saveObject.simName;
+        persistObject.created = rightNow().toLocaleString();
+        persistObject.modified = saveObject.created;
+        persistObject.config = JSON.stringify(saveObject);
+        
+        if(saveTowns){
+                saveObject.towns = JSON.stringify(towns);
+        }
+        else{
+                saveObject.towns = "";
+        }
+        
+        return persistObject;
+}
+
+function updatePersistObject(persistObject, saveTowns){
+        var saveObject = generateConfigObject();
+        if(persistObject.name != saveObject.simName){
+                persistObject.name = saveObject.simName;
+        }
+        
+        persistObject.modified = rightNow().toLocaleString();
+        persistObject.config = JSON.stringify(saveObject);
+        
+        if(saveTowns){
+                saveObject.towns = JSON.stringify(towns);
+        }
+        
+        return persistObject;
+}
+
+function getPersistObjects(){
+        var numEntries = localStorage.getItem('numEntries');
+        
+        if(numEntries === null){
+                console.log("no persistent entries found");
+                return null;
+        }
+        
+        var entries = [];
+        for(var i = 0; i < numEntries; i++){
+                var entry = localStorage.getItem('entry' + i);
+                console.log("entry");
+                entries.push(JSON.parse(entry));
+        }
+        
+        return entries;
+}
+
+function setupPersistConfigs(){
+        var entries = getPersistObjects();
+        if(!entries){
+                document.getElementById("persistMessage").innerHTML = "No existing saves found";
+                return;
+        }
+        
+        document.getElementById("persistMessage").innerHTML = "Found " + entries.length + " saved simulation(s).";
+}
+
 checkCompatibility();
+setupPersistConfigs();
